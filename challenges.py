6ad8ecd170d40eda391052cc5ceb878c1cdc64ab
Lauren Zielinski <laurzie@umich.edu>
2018-09-10 09:44:26 -0400

    first push

--- challenges.py
+++ challenges.py
@@ -14,12 +14,15 @@ def courseView(course_name):
 #Task 3.1 Basic HTML Form
     #Set the method and action of the HTML form, such that form data is sent to /result using POST method
     #The form should have a text field in which you can enter an ingredient (milk, eggs, etc)
-@app.route('/form')
+@app.route('/form') #the url you type in localhost:5000/form
 def formView():
     html_form = '''
     <html>
     <body>
-    <form>
+    <form method = "GET" action ="http://localhost:5000/result">
+    Ingredient :
+    <input type= "text" name="ingredient" ></input>
+    <input type = "submit" name= "submit"></input>
     </form>
     </body>
     </html>
@@ -29,8 +32,10 @@ def formView():
 #Task 3.2 : Processing Form Data
 @app.route('/result', methods = ['GET', 'POST'])
 def resultView():
+    if request.method == "GET":
+        ingg = request.args.get("ingredient")
     # Make an API request to Recipe API for the ingredient entered in the form and display the recipe results
-    return ""
+    return ingg
 
 
 if __name__ == '__main__':
